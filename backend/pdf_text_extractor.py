58c531393246622e2d5f8c09f0e8ff5839ba0d0a
NeelimaGurram <gurramneelima03@gmail.com>
2026-06-11 19:21:15 -0400

    feat: add regex extractors for email, phone, linkedin

--- backend/pdf_text_extractor.py
+++ backend/pdf_text_extractor.py
@@ -1,8 +1,10 @@
 import json
 import pdfplumber
+import re
+from typing import Optional
 
 
-pdf_path="../data/resume.pdf"
+pdf_path="data/resume.pdf"
 
 def extract_text_with_pdfplumber(pdf_path: str)-> dict:
     result={"file":pdf_path,
@@ -22,13 +24,46 @@ def extract_text_with_pdfplumber(pdf_path: str)-> dict:
     
     return result
 
-def main():
-    text=extract_text_with_pdfplumber(pdf_path)
+def extract_email(text: str)->Optional[str]:
+    pattern=r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+    match=re.search(pattern, text)
+    return match.group() if match else None
 
-    print(json.dumps(text, indent=2))
+def extract_phone(text: str)-> Optional[str]:
+    pattern=r"""
+        (\+\d{1,3}[\s-])?
+        \(?\d{3}\)?
+        [\s.\-]?
+        \d{3}
+        [\s.\-]?
+        \d{4}
+     """
+    match=re.search(pattern, text,re.VERBOSE)
+    return match.group().strip() if match else None
+
+def extract_linkedin(text:str)->Optional[str]:
+    pattern=r"(https?://)?(www\.)?linkedin\.com/in/[a-zA-Z0-9_-]+/?"
+    match=re.search(pattern, text,re.IGNORECASE)
+    return match.group() if match else None
+
+def extract_contact_info(text:str)->dict:
+    return {
+        "email":extract_email(text),
+        "phone":extract_phone(text),
+        "linkedin":extract_linkedin(text)
+    }
+
+
+def main():
+    extracted=extract_text_with_pdfplumber(pdf_path)
+    full_text=extracted["full_text"]
+
+    extracted["contact_info"]=extract_contact_info(full_text)
+
+    print(json.dumps(extracted, indent=2))
 
     with open("resume_output.json","w") as f:
-        json.dump(text,f, indent=2)
+        json.dump(extracted,f, indent=2)
     print("\n saved: resume_output.json")
 
 if __name__ =="__main__":
